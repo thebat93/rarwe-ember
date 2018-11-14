98795d6e83ad2773fa73f915384d482f18c784a3
thebat93 <ais_93@mail.ru>
2018-11-14 12:22:07 +0300

    Smoother UI Flow

--- app/controllers/bands.js
+++ app/controllers/bands.js
@@ -1,5 +1,6 @@
 import Controller from '@ember/controller';
 import Band from 'rarwe/models/band';
+// хелпер который возвращает true если переданное свойство пустое
 import { empty } from '@ember/object/computed';
 
 export default Controller.extend({
@@ -20,7 +21,13 @@ export default Controller.extend({
             e.preventDefault();
             let newBand = Band.create({ name: this.newBandName });
             this.model.pushObject(newBand);
-            this.set('newBandName', '');
+            // обнуляем ввод и возвращаем в изначальное состояние
+            this.setProperties({
+                newBandName: '',
+                isAddingBand: false
+            });
+            // переходим по роуту с band = newBand
+            this.transitionToRoute('bands.band.songs', newBand);
         }
     }
 });
