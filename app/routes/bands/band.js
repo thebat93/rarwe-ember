110b9f3d2442c6b987c6809ede490f505c9f242b
thebat93 <ais_93@mail.ru>
2018-11-17 17:51:32 +0300

    Added 'details' tab template

--- app/routes/bands/band.js
+++ app/routes/bands/band.js
@@ -1,8 +1,19 @@
 import Route from '@ember/routing/route';
+import { isEmpty } from '@ember/utils';
 
 export default Route.extend({
     model(params) {
         let bands = this.modelFor('bands');
         return bands.findBy('slug', params.slug);
+    },
+
+    // хук редиректа, выполняется после окончания перехода на роут
+    redirect(band) {
+        // переходим на тот или иной роут в зависимости от наличия описания
+        if (band.description) {
+            this.transitionTo('bands.band.details');
+        } else {
+            this.transitionTo('bands.band.songs');
+        }
     }
 });
