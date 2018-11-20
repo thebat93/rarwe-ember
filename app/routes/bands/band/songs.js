d93e819c9f64ce573bbb86e442de954fd3dbc010
thebat93 <ais_93@mail.ru>
2018-11-20 10:46:09 +0300

    Fix bug of preserving song name

--- app/routes/bands/band/songs.js
+++ app/routes/bands/band/songs.js
@@ -1,6 +1,13 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+    // хук, который вызывается когда происходит выход из роута или меняется модель
+    resetController(controller/*, isExiting, transition*/) {
+        controller.setProperties({
+            isAddingSong: false,
+            newSongName: ''
+        });
+    },
     model() {
         return this.modelFor('bands.band')
     }
