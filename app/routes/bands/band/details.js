a804add7daf2d99eec647bcaf351c010bd3ab8a7
Igor <ais_93@mail.ru>
2018-11-20 23:55:13 +0300

    Warning about losing data

--- app/routes/bands/band/details.js
+++ app/routes/bands/band/details.js
@@ -1,8 +1,30 @@
-import Route from '@ember/routing/route';
+import Route from "@ember/routing/route";
 
 export default Route.extend({
-    // подразумевается по умолчанию
-    // model() {
-    //     return this.modelFor('bands.band');
-    // }
+  // подразумевается по умолчанию
+  // model() {
+  //     return this.modelFor('bands.band');
+  // }
+
+  // хук, который вызывается когда происходит выход из роута
+  resetController(controller, isExiting) {
+    if (isExiting) {
+      controller.setProperties({
+        isEditing: false
+      });
+    }
+  },
+
+  actions: {
+    // вызывается перед переходом на другой роут
+    willTransition(transition) {
+      if (this.controller.isEditing) {
+        let leave = window.confirm("Are you sure?");
+        if (!leave) {
+          // отмена перехода
+          transition.abort();
+        }
+      }
+    }
+  }
 });
