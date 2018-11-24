45d116330df3f1849d8dd30159e2a87ab2c41de7
thebat93 <ais_93@mail.ru>
2018-11-24 21:50:39 +0300

    Lint errors fix

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -3,43 +3,43 @@ import Controller from '@ember/controller';
 import { empty } from '@ember/object/computed';
 
 export default Controller.extend({
-    isAddingSong: false,
-    newSongName: '',
-    isAddButtonDisabled: empty('newSongName'),
+  isAddingSong: false,
+  newSongName: '',
+  isAddButtonDisabled: empty('newSongName'),
 
-    actions: {
-        // добавить новую песню
-        addSong() {
-            this.set('isAddingSong', true);
-        },
+  actions: {
+    // добавить новую песню
+    addSong() {
+      this.set('isAddingSong', true);
+    },
 
-        //отменить добавление новой песни
-        cancelAddSong() {
-            this.set('isAddingSong', false);
-        },
+    //отменить добавление новой песни
+    cancelAddSong() {
+      this.set('isAddingSong', false);
+    },
 
-        // сохранить новую песню
-        async saveSong(e) {
-            e.preventDefault();
-            // создать новую запись в сторе
-            let newSong = this.get('store').createRecord('song', {
-                title: this.get('newSongName'),
-                // связь с группой
-                band: this.model
-            });
-            // POST-запрос
-            await newSong.save();
-            // обнуляем название новой песни
-            this.set('newSongName', '');
-        },
+    // сохранить новую песню
+    async saveSong(e) {
+      e.preventDefault();
+      // создать новую запись в сторе
+      let newSong = this.get('store').createRecord('song', {
+        title: this.get('newSongName'),
+        // связь с группой
+        band: this.model
+      });
+      // POST-запрос
+      await newSong.save();
+      // обнуляем название новой песни
+      this.set('newSongName', '');
+    },
 
-        // обновить рейтинг
-        updateRating(song, rating) {
-            // обновляем свойство модели
-            song.set('rating', song.rating === rating ? 0 : rating);
+    // обновить рейтинг
+    updateRating(song, rating) {
+      // обновляем свойство модели
+      song.set('rating', song.rating === rating ? 0 : rating);
 
-            // PATCH-запрос на обновление рейтинга
-            return song.save();
-        }
+      // PATCH-запрос на обновление рейтинга
+      return song.save();
     }
+  }
 });
--- app/routes/bands/band.js
+++ app/routes/bands/band.js
@@ -1,5 +1,4 @@
 import Route from '@ember/routing/route';
-import { isEmpty } from '@ember/utils';
 
 export default Route.extend({
     // получает в качестве аргументов params и transition
