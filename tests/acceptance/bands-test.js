81d59b8b25fd4f2d3b10dde7b01b8507d1bdcc1b
thebat93 <ais_93@mail.ru>
2018-11-25 21:28:29 +0300

    Added test for creating a band

--- tests/acceptance/bands-test.js
+++ tests/acceptance/bands-test.js
@@ -1,5 +1,5 @@
 import { module, test } from 'qunit';
-import { visit } from '@ember/test-helpers';
+import { visit, click, fillIn } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import setupMirageTest from 'ember-cli-mirage/test-support/setup-mirage';
 
@@ -17,5 +17,19 @@ module('Acceptance | Bands', function(hooks) {
     assert.equal(bandLinks.length, 2, 'All band links are rendered');
     assert.ok(bandLinks[0].textContent.includes('Radiohead'), 'First band link contains the band name');
     assert.ok(bandLinks[1].textContent.includes('Long Distance Calling'), 'The other band link contains the band name');
+  });
+
+  test('Create a band', async function(assert) {
+    this.server.create('band', { name: 'Royal Blood' });
+    
+    await visit('/');
+    await click('label');
+    await fillIn('.rr-input', 'Caspian');
+    await click('.rr-action-button');
+    
+    let bandLinks = document.querySelectorAll('.rr-band-link');
+    assert.equal(bandLinks.length, 2, 'All band links are rendered', 'A new band link is rendered');
+    assert.ok(bandLinks[1].textContent.includes('Caspian'), 'The new band link is rendered as the last item');
+    assert.ok(document.querySelector('.rr-navbar-item > .active').textContent.includes('Songs'), 'The songs tab is active');
   });
 });
