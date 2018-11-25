07ed7b1bd4656c3a46871589ef24765894b401f2
thebat93 <ais_93@mail.ru>
2018-11-25 21:16:18 +0300

    Added test for visiting the bands page

---
+++ tests/acceptance/bands-test.js
@@ -0,0 +1,21 @@
+import { module, test } from 'qunit';
+import { visit } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+import setupMirageTest from 'ember-cli-mirage/test-support/setup-mirage';
+
+module('Acceptance | Bands', function(hooks) {
+  setupApplicationTest(hooks);
+  setupMirageTest(hooks);
+
+  test('List bands', async function(assert) {
+    this.server.create('band', { name: 'Radiohead' });
+    this.server.create('band', { name: 'Long Distance Calling' });
+    
+    await visit('/');
+    
+    let bandLinks = document.querySelectorAll('.rr-band-link');
+    assert.equal(bandLinks.length, 2, 'All band links are rendered');
+    assert.ok(bandLinks[0].textContent.includes('Radiohead'), 'First band link contains the band name');
+    assert.ok(bandLinks[1].textContent.includes('Long Distance Calling'), 'The other band link contains the band name');
+  });
+});
