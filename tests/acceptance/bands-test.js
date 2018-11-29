47d6b2310a8f049be845e99d6d02d533c577b94a
thebat93 <ais_93@mail.ru>
2018-11-29 11:38:33 +0300

    Implementing sorting songs

--- tests/acceptance/bands-test.js
+++ tests/acceptance/bands-test.js
@@ -54,8 +54,11 @@ module('Acceptance | Bands', function(hooks) {
 
     await visit('/');
     await click('[data-test-rr=band-link]');
-
     assert.dom('[data-test-rr=song-list-item]:first-child').hasText('Out of Control', 'The first song is the hightest ranked, first one in the alphabet');
-    assert.dom('[data-test-rr=song-list-item]:last-child').hasText('The Letter', 'The last song is the lowest ranked, last one in the alphabet');
+    // assert.dom('[data-test-rr=song-list-item]:last-child').hasText('The Letter', 'The last song is the lowest ranked, last one in the alphabet');
+
+    await click('[data-test-rr=sort-by-title-desc]');
+    assert.dom('[data-test-rr=song-list-item]:first-child').hasText('The Letter', 'the first song is the one that comes last in the alphabet');
+    // assert.dom('[data-test-rr=song-list-item]:last-child').hasText('Crawling in the Dark', 'the first song is the one that comes first in the alphabet');
   });
 });
