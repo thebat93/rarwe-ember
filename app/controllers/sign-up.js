a9a7a3e0b99371451f32f2aa893d8dd314ccee96
thebat93 <ais_93@mail.ru>
2018-12-08 19:32:45 +0300

    Sign Up page

---
+++ app/controllers/sign-up.js
@@ -0,0 +1,17 @@
+import Controller from '@ember/controller';
+
+export default Controller.extend({
+    actions: {
+      // зарегистрироваться
+      async signUp(e) {
+        e.preventDefault();
+        let { email, password } = this;
+        // создаем новую запись в сторе...
+        let user = this.store.createRecord('user', { email, password });
+        // ...и сохраняем ее, отправляя запрос на сервер
+        await user.save();
+        // переходим на страницу логина
+        await this.transitionToRoute('login');
+      }
+    }
+});
