e3cf1bfe4c3e31de3c9bb1a5b330f2a3cb860545
Igor <ais_93@mail.ru>
2018-12-16 16:15:22 +0300

    Redirecting from unprotected pages if logged in

--- app/routes/login.js
+++ app/routes/login.js
@@ -1,4 +1,6 @@
 import Route from '@ember/routing/route';
+import UnauthenticatedRouteMixin from 'ember-simple-auth/mixins/unauthenticated-route-mixin';
 
-export default Route.extend({
+// миксин для незащищенного роута: редирект если пользователь авторизован
+export default Route.extend(UnauthenticatedRouteMixin, {
 });
--- app/routes/sign-up.js
+++ app/routes/sign-up.js
@@ -1,4 +1,7 @@
 import Route from '@ember/routing/route';
+import UnauthenticatedRouteMixin from 'ember-simple-auth/mixins/unauthenticated-route-mixin';
 
-export default Route.extend({
+// миксин для незащищенного роута: редирект если пользователь авторизован
+// по дефолту редирект на /index
+export default Route.extend(UnauthenticatedRouteMixin, {
 });
