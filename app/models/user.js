de58dfe2a293df78280d0a68013f1e435895791b
Igor <ais_93@mail.ru>
2018-12-16 22:52:58 +0300

    Field validations for signup form

--- app/models/user.js
+++ app/models/user.js
@@ -1,8 +1,35 @@
 import DS from 'ember-data';
+import { buildValidations, validator } from 'ember-cp-validations';
 
 const { Model, attr } = DS;
 
-export default Model.extend({
+// конфигурация валидаций полей
+const Validations = buildValidations({
+  email: [
+    validator('presence', {
+      presence: true,
+      ignoreBlank: true,
+      message: 'Email can\'t be empty'
+    }),
+    validator('format', {
+      type: 'email',
+      message: 'Email should be a valid email'
+    })
+  ],
+  password: [
+    validator('presence', {
+      presence: true,
+      ignoreBlank: true,
+      message: 'Password can\'t be empty'
+    }),
+    validator('length', {
+      min: '8',
+      message: 'Password should be at least 8 characters long'
+    })
+  ]
+});
+
+export default Model.extend(Validations, {
   email: attr('string'),
   password: attr('string')
 });
