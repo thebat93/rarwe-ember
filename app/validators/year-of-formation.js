7bbef4fc6ee914a5d0c173ac1965735d1f5d00f3
thebat93 <ais_93@mail.ru>
2019-01-03 20:50:47 +0700

    Реализация метода validate кастомного валидатора

--- app/validators/year-of-formation.js
+++ app/validators/year-of-formation.js
@@ -1,8 +1,24 @@
 import BaseValidator from 'ember-cp-validations/validators/base';
 
 const Description = BaseValidator.extend({
-  validate(/*value, options, model, attribute*/) {
-    return true;
+  // Функция возвращает true если поле валидно или сообщение об ошибке
+  validate(value) {
+    // Делим описание на слова
+    let words = value.split(/\s+/);
+    // Определяем текущий год
+    let currentYear = new Date().getFullYear();
+    // Определяем год образования, указанный в описании
+    let yearOfFormation = words.find((word) => {
+      // Находим слова из четырех цифр
+      if (word.match(/\b\d{4}\b/)) {
+        let year = parseInt(word, 10);
+        // Валидируем цифру:
+        // Год образования должен быть больше 1900
+        // и меньше или равен текущему году
+        return year > 1900 && year <= currentYear;
+      }
+    });
+    return yearOfFormation ? true : 'The year of formation must be included in the description';
   }
 });
 
