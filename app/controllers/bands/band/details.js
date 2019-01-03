4e13cff6c8e83a12d1027650301865e6075c7bcf
thebat93 <ais_93@mail.ru>
2019-01-03 23:17:57 +0700

    Реализация валидации в контроллере 'details'

--- app/controllers/bands/band/details.js
+++ app/controllers/bands/band/details.js
@@ -1,4 +1,5 @@
 import Controller from "@ember/controller";
+import { computed } from '@ember/object';
 
 export default Controller.extend({
   isEditing: false,
@@ -8,15 +9,30 @@ export default Controller.extend({
     edit() {
       this.set('isEditing', true);
     },
+  
+    showErrors: computed('_showErrors', {
+      get() {
+        return this._showErrors || { description: false };
+      },
+      set(key, value) {
+        this.set('_showErrors', value);
+        return this._showErrors;
+      }
+    }),
 
     // обновить (сохранить) описание
     async save() {
       // модель = группа
       let band = this.model;
-      // PATCH-запрос
-      await band.save();
-      // сбрасываем флаг редактирования
-      this.set('isEditing', false);
+      // При сохранении описания можно отображать ошибки валидации
+      this.set('showErrors.description', true);
+
+      if (band.validations.isValid) {
+        // PATCH-запрос
+        await band.save();
+        // сбрасываем флаг редактирования
+        this.set('isEditing', false);
+      }
     }
 
     // toggleIsEditing() {
